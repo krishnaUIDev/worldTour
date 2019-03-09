cdf26a4c6aa24459048b8ee5a2b1eb8c18f02667
krishnaUIDev <krishna.kondoju007@gmail.com>
2019-03-09 15:33:42 -0500

    shapes added

--- index.vr.js
+++ index.vr.js
@@ -1,34 +1,149 @@
-import React from 'react';
-import {
-  AppRegistry,
-  asset,
-  Pano,
-  Text,
-  View,
-} from 'react-vr';
+import React, { Component } from 'react'
 
-export default class WorldTour extends React.Component {
-  render() {
+import {
+  View,
+  Text,
+  Pano,
+  AppRegistry,
+  Cylinder,
+  Box,
+  asset,
+  StyleSheet
+} from 'react-vr'
+
+const places = [
+  {
+    title: 'Island Paradise',
+    image: 'zion-vr.jpg'
+  },
+  {
+    title: 'Starry Night',
+    image: 'island-garden.jpg'
+  },
+  {
+    title: 'Winter Outdoors',
+    image: 'lombard-vr.jpg'
+  },
+  {
+    title: 'Light Show',
+    image: 'museum.jpg'
+  }
+]
+
+class WorldTour extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      showMenu: false,
+      place: 'zion-vr.jpg'
+    }
+  }
+
+  toggleMenu () {
+    this.setState({ showMenu: !this.state.showMenu })
+  }
+
+  render () {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
+        <Pano source={asset(this.state.place)} />
+        <View style={styles.menuButton} onEnter={() => this.toggleMenu()}>
+          <Text style={styles.menuButtonText}>
+            {this.state.showMenu ? 'Close Menu' : 'Open Menu'}
+          </Text>
+        </View>
+        <Box
+          dimWidth={0.5}
+          dimHeight={0.5}
+          dimDepth={0.5}
+          wireframe
           style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          erwe
-        </Text>
+            color: '#dd2222',
+            transform: [
+              { translate: [0, 0, -3] },
+              { translateY: 1 },
+              { translateX: -0.5 },
+              { rotateY: 45 },
+              { rotateZ: 45 }
+            ]
+          }}
+        />
+        <Cylinder
+          dimWidth={0.5}
+          dimHeight={0.5}
+          dimDepth={0.5}
+          wireframe
+          segments={50}
+          style={{
+            color: '#2d2',
+            transform: [
+              { translate: [0, 0.5, -3] },
+              { rotateY: 45 },
+              { rotateZ: 45 },
+              { rotateX: 50 }
+            ]
+          }}
+        />
+        {this.state.showMenu ? (
+          <View style={styles.menu}>
+            {places.map((place, index) => {
+              return (
+                <View
+                  style={styles.menuItem}
+                  key={index}
+                  onEnter={() => this.setState({ place: place.image })}
+                >
+                  <Text style={styles.menuItemText}>{place.title}</Text>
+                </View>
+              )
+            })}
+          </View>
+        ) : (
+          <View />
+        )}
       </View>
-    );
+    )
   }
-};
+}
 
-AppRegistry.registerComponent('WorldTour', () => WorldTour);
+const styles = StyleSheet.create({
+  menu: {
+    width: 5,
+    height: 1.25,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    transform: [{ translate: [-2, 0, -7.5] }]
+  },
+  menuButton: {
+    backgroundColor: '#fff',
+    borderRadius: 0.25,
+    width: 0.5,
+    height: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 0.01,
+    transform: [{ translate: [-2, 0, -5] }]
+  },
+  menuButtonText: {
+    textAlign: 'center',
+    fontSize: 0.15,
+    color: '#000'
+  },
+  menuItem: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 1,
+    height: 1,
+    borderRadius: 0.5,
+    borderWidth: 0.02,
+    backgroundColor: '#fff'
+  },
+  menuItemText: {
+    fontSize: 0.2,
+    textAlign: 'center',
+    color: '#000'
+  }
+})
+AppRegistry.registerComponent('WorldTour', () => WorldTour)
